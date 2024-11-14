398c1dbfee394ef98a4e8bec29b702ad8b0c47b6
Cyrille <cyrille.burtin@me.com>
2024-11-14 16:48:39 +0100

    [CHORE] update gitignore

--- src/features/user/api/action.ts
+++ src/features/user/api/action.ts
@@ -1,4 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 export const getUser = async () => {
-
-
-}
+  try {
+    const users = prisma.user.findMany();
+    console.log(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw new Error("Failed to fetch users");
+  } finally {
+    await prisma.$disconnect();
+  }
+};
